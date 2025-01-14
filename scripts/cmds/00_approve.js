cd041e5bd44e9c360b5a9892ea0beb82afeb9ebf
rebusukianya <rebusukianya@gmail.com>
2025-01-14 22:48:52 +0600

    Update approve.js

--- scripts/cmds/00_approve.js
+++ scripts/cmds/00_approve.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 module.exports = {
 	config: {
-		name: "approved",
-		author: "Jun",
+		name: "approve",
+		author: "Jun & Aljur",
 		countDown: 5,
 		role: 0,
 		category: "owner",
